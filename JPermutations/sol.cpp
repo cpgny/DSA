088e1bbf35177da36b40e40d4f02ceea8790f26d
cpgny <86189916+cpgny@users.noreply.github.com>
2022-02-13 19:10:13 +0400

    Update sol.cpp

--- JPermutations/sol.cpp
+++ JPermutations/sol.cpp
@@ -106,6 +106,47 @@ void sol(vector<int> items, int k) {
     return;
 }
 
+//Segment Tree O(NlogN)
+const int N = 1 << 18;
+int T[N+N];	
+
+void init(int n) {
+    for(int i = 0; i < n; ++i) {
+       T[i + N] = 1;
+    }
+    for(int i = N - 1; i > 0; --i) {
+       T[i] = T[i << 1] + T[i << 1 | 1];
+    }
+}
+
+int lower_bound(int x, int p = 1) {
+    while(p < N) {
+       if(T[p <<= 1] < x) {
+          x -= T[p++];
+       }
+    }
+    return p - N;
+}
+
+void update(int p, int v) {
+    for(T[p += N] = v; p > 1; p >>= 1) {
+       T[p >> 1] = T[p] + T[p ^ 1];
+    }
+}
+
+void sol(int n, int k) {
+    rng
+    init(n);
+    for(int remain = n, pos = 0; remain > 0; --remain) {
+       pos += remain + k;
+       pos %= remain;
+       int p = lower_bound(pos + 1);
+       cout << p + 1 << " ";
+       update(p, 0);
+    }
+    return;
+}
+
 int main() {
     int k;
     read(k);
